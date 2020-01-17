6a7129ae2d5c05defd59a586fe9ca18ba7f765eb
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2020-01-17 15:53:56 -0500

    add another year data and compare

--- exam_trend.py
+++ exam_trend.py
@@ -3,14 +3,21 @@ from matplotlib import pyplot as plt
 #months 
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+#of exams in 2018
+num_of_exams_18 = [9122, 9941, 8033, 9010, 8015, 9090, 8994, 8015 , 9745, 8320, 9511, 9501]
 #of exams in 2019
-num_of_exams = [8522, 7941, 9033, 9060, 10015, 8090, 7994, 9015 , 10745, 9320, 10511, 9521]
+num_of_exams_19 = [8522, 7941, 9033, 9060, 10015, 8090, 7994, 9015 , 10745, 9320, 10511, 9521]
 
-plt.plot(months, num_of_exams)
+#of exams in 2018
+
+plt.plot(months, num_of_exams_18, color='m', linestyle='--', label='2018')
+plt.plot(months, num_of_exams_19, color='k', linestyle='-', label='2019')
 
 plt.xlabel('Months')
 plt.ylabel('# of Exams')
 
 plt.title('Exam count trend by month')
+
+plt.legend()
 
 plt.show()
